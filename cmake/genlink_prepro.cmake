c986bb4046c79ecb5f7038c197fba0acd5c335e3
KitsuneAlex <alexh@karmakrafts.dev>
2026-04-13 17:04:21 +0200

    Add debug output for genlink_prepro

--- cmake/genlink_prepro.cmake
+++ cmake/genlink_prepro.cmake
@@ -12,10 +12,10 @@ cmake_minimum_required(VERSION 3.18)
 include(genlink)
 
 function(_genlink_preprocess TEMPLATE SCRIPT RESULT)
+    message(STATUS "Invoking ${CMAKE_C_COMPILER} ${_LOCM3_ARCH_FLAGS} ${_LOCM3_DEVICE_DEFS} -P -E ${TEMPLATE} -o ${SCRIPT}")
     execute_process(COMMAND ${CMAKE_C_COMPILER}
             ${_LOCM3_ARCH_FLAGS} ${_LOCM3_DEVICE_DEFS}
             -P -E ${TEMPLATE}
             -o ${SCRIPT}
-            RESULT_VARIABLE ${RESULT}
-            OUTPUT_VARIABLE ${RESULT}_OUTPUT)
+            RESULT_VARIABLE ${RESULT})
 endfunction()
