ff85b99fc0e5d8c2c0db2de09f51730b6007b739
KitsuneAlex <alexh@karmakrafts.dev>
2026-04-13 16:59:29 +0200

    Export stdout result from genlink prepro to variable in CMake module

--- cmake/genlink_prepro.cmake
+++ cmake/genlink_prepro.cmake
@@ -16,5 +16,6 @@ function(_genlink_preprocess TEMPLATE SCRIPT RESULT)
             ${_LOCM3_ARCH_FLAGS} ${_LOCM3_DEVICE_DEFS}
             -P -E ${TEMPLATE}
             -o ${SCRIPT}
-            RESULT_VARIABLE ${RESULT})
+            RESULT_VARIABLE ${RESULT}
+            OUTPUT_VARIABLE ${${RESULT}_OUTPUT})
 endfunction()
